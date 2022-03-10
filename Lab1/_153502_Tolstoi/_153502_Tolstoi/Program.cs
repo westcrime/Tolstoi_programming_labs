0915830b2b4e83b08c37089dff23cc2c9c2bf75e
westcrime <westcrime77@rambler.ru>
2022-03-10 10:11:14 +0300

    added 1 lab

---
+++ Lab1/_153502_Tolstoi/_153502_Tolstoi/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+    class _153502_Tolstoi
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0;
+
+            System.Console.WriteLine("Enter 2 numbers");
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            double answer = x / y;
+
+            System.Console.WriteLine("Answer is ");
+            System.Console.WriteLine(answer);
+        }
+    }
+}
