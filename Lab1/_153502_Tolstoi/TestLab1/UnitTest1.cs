e85d39c9aeb68d9936532fa7e9b5f233330eb620
westcrime <westcrime77@rambler.ru>
2022-03-10 12:37:06 +0300

    task is changed in better way

--- Lab1/_153502_Tolstoi/TestLab1/UnitTest1.cs
+++ Lab1/_153502_Tolstoi/TestLab1/UnitTest1.cs
@@ -10,11 +10,29 @@ namespace TestLab1
         [TestMethod]
         public void TestMethod1()
         {
+            var mc = new _153502_Tolstoi();
+
             int a = 6, b = 3;
 
-            double ans = a / b;
+            double ans = mc.task(a, b);
 
             Assert.AreEqual(2, ans);
+        }
+    }
+
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var mc = new _153502_Tolstoi();
+
+            int a = 81, b = 3;
+
+            double ans = mc.task(a, b);
+
+            Assert.AreEqual(27, ans);
         }
     }
 }
--- Lab1/_153502_Tolstoi/_153502_Tolstoi/Program.cs
+++ Lab1/_153502_Tolstoi/_153502_Tolstoi/Program.cs
@@ -1,9 +1,11 @@
 ﻿namespace HelloWorld
 {
-    class _153502_Tolstoi
+    public class _153502_Tolstoi
     {
         static void Main(string[] args)
         {
+            var mc = new _153502_Tolstoi();
+
             double x = 0, y = 0;
 
             System.Console.WriteLine("Enter 2 numbers");
@@ -11,10 +13,15 @@
             x = Convert.ToInt32(Console.ReadLine());
             y = Convert.ToInt32(Console.ReadLine());
 
-            double answer = x / y;
+            double answer = mc.task(x, y);
 
             System.Console.WriteLine("Answer is ");
             System.Console.WriteLine(answer);
+        }
+
+        public double task(double a, double b)
+        {
+            return a / b;
         }
     }
 }
