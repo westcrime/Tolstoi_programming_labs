45205658a3e46d1e715ee60b6ff2045b78ce5c02
westcrime <westcrime77@rambler.ru>
2022-04-27 21:23:37 +0300

    added lab7

---
+++ Lab7/Solution/Task/Equation.cs
@@ -0,0 +1,133 @@
+﻿namespace Task;
+
+public class Equation
+{
+    private int a;
+    private int b;
+    private int c;
+
+    public Equation()
+    {
+        A = 0;
+        B = 0;
+        C = 0;
+    }
+
+    public Equation(int a, int b, int c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public void GetSolutions()
+    {
+        var d = (int) Math.Pow(b, 2) - 4 * a * c;
+
+        if (d < 0)
+        {
+            Console.WriteLine("This equation does not have solutions!");
+            return;
+        }
+
+        if (d == 0)
+        {
+            var solution = -1 * (b / (2.0 * a));
+            Console.WriteLine($"The solution is {solution}.");
+            return;
+        }
+
+        var solution1 = (-b + Math.Sqrt(d)) / (2 * a);
+        var solution2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+        Console.WriteLine($"The solutions are {solution1} and {solution2}.");
+    }
+
+    public int A
+    {
+        get => a;
+        private set => a = value;
+    }
+
+    public int B
+    {
+        get => b;
+        private set => b = value;
+    }
+
+    public int C
+    {
+        get => c;
+        private set => c = value;
+    }
+
+    // Дальше перегрузка операторов
+    
+    public static Equation operator +(Equation eq, int value)
+    {
+        return new Equation(eq.A, eq.B, eq.C + value);
+    }
+
+    public static Equation operator -(Equation eq, int value)
+    {
+        return new Equation(eq.A, eq.B, eq.C - value);
+    }
+
+    public static Equation operator +(Equation eq1, Equation eq2)
+    {
+        return new Equation(eq1.A + eq2.A, eq1.B + eq2.B, eq1.C + eq2.C);
+    }
+    
+    public static Equation operator -(Equation eq1, Equation eq2)
+    {
+        return new Equation(eq1.A - eq2.A, eq1.B - eq2.B, eq1.C - eq2.C);
+    }
+    
+    public static Equation operator ++(Equation eq)
+    {
+        return new Equation(eq.A, eq.B, eq.C + 1);
+    }
+
+    public static Equation operator --(Equation eq)
+    {
+        return new Equation(eq.A, eq.B, eq.C - 1);
+    }
+
+    public static Equation operator *(Equation eq, int value)
+    {
+        return new Equation(eq.A * value, eq.B * value, eq.C * value);
+    }
+
+    public static Equation operator /(Equation eq, int value)
+    {
+        return new Equation(eq.A / value, eq.B / value, eq.C / value);
+    }
+
+    public static bool operator false(Equation eq)
+    {
+        var d = (int) Math.Pow(eq.B, 2) - 4 * eq.A * eq.C;
+        return d < 0;
+    }
+    
+    public static bool operator true(Equation eq)
+    {
+        var d = (int) Math.Pow(eq.B, 2) - 4 * eq.A * eq.C;
+        return d >= 0;
+    }
+
+    public static bool operator !(Equation eq)
+    {
+        var d = (int) Math.Pow(eq.B, 2) - 4 * eq.A * eq.C;
+        return !(d >= 0);
+    }
+
+    public static bool operator ==(Equation eq1, Equation eq2)
+    {
+        return (eq1.A == eq2.A && eq1.B == eq2.B && eq1.C == eq2.C);
+    }
+    
+    public static bool operator !=(Equation eq1, Equation eq2)
+    {
+        return (eq1.A != eq2.A || eq1.B != eq2.B || eq1.C != eq2.C);
+    }
+}
---
+++ Lab7/Solution/Task/Program.cs
@@ -0,0 +1,26 @@
+﻿using Task;
+
+var eq1 = new Equation(3, -14, -5);
+eq1.GetSolutions();
+
+var eq2 = new Equation(1, 1, 1);
+var eq3 = new Equation(1, 1, 1);
+if (eq2 == eq3) Console.WriteLine("eq2 and eq3 are equal.");
+
+var eq4 = new Equation(1, 1, 1);
+var eq5 = new Equation(-10, 5, -2);
+var eq6 = eq4 + eq5; // -9x^2 + 6x - 1 = 0
+eq6.GetSolutions();
+
+var eq7 = new Equation(6, 1, 8);
+if (!eq7) Console.WriteLine("eq7 does not have solutions");
+
+eq6--;
+eq6++;
+
+eq6.GetSolutions();
+
+eq6 *= 2;
+eq6 /= 2;
+
+eq6.GetSolutions();
